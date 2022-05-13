080339e18921169d35e69f05514a32e56e5e9938
Ken Fukuyama <kenfdev@gmail.com>
2022-05-13 13:21:10 +0900

    add security resources

---
+++ 30_security/vite-project/src/App.vue
@@ -0,0 +1,61 @@
+<script setup>
+import { ref } from 'vue';
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import HelloWorld from './components/HelloWorld.vue'
+import DangerousHtml from './dangerousHtml.json';
+
+const msg = ref('Hello Vue 3 + Vite')
+const anchorNg = ref('javascript:alert("危険なaタグ！")')
+
+function vHtmlOk() {
+  msg.value = "<h1>安全なHTML</h1>"
+}
+
+function vHtmlNg() {
+  msg.value = DangerousHtml.html
+}
+
+</script>
+
+<template>
+  <div>
+    <img alt="Vue logo" src="./assets/logo.png" />
+  </div>
+  <div>
+    <h2>v-html</h2>
+    <div class="v-html-button-area">
+      <button @click="vHtmlOk">大丈夫なv-html</button>
+      <button @click="vHtmlNg">脆弱なv-html</button>
+    </div>
+  </div>
+  <HelloWorld :msg="msg" />
+  <div>
+    <h2>aタグ</h2>
+    <div class="a-area">
+      <a :href="anchorNg">脆弱なaタグ</a>
+    </div>
+  </div>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+.v-html-button-area {
+  display: flex;
+  justify-content: center;
+  gap: 1rem;
+  margin-bottom: 1rem;
+}
+
+.a-area {
+  margin-bottom: 1rem;
+}
+</style>
---
+++ 30_security/vite-project/src/components/HelloWorld.vue
@@ -0,0 +1,16 @@
+<script setup>
+defineProps({
+  msg: String
+})
+
+</script>
+
+<template>
+  <div v-html="msg"></div>
+</template>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+</style>
